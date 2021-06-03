b85b13cfbfc1645e1032fbe379d6dd422a208868
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-03 17:14:53 +0300

    Completed exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,21 +1,31 @@
 import React from 'react'
-const Header = (props) =>{
+const Header = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
     </div>
   )
 }
-const Content = (props)=>{
-  return(
+const Part = (props) => {
+  return (
     <p>
-        {props.part} {props.exercise}
+      {props.name} {props.exercise}
     </p>
   )
 }
 
-const Total = (props)=>{
-  return(
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.name1} exercise={props.ex1}></Part>
+      <Part name={props.name2} exercise={props.ex2}></Part>
+      <Part name={props.name3} exercise={props.ex3}></Part>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
     <p>
       Number of exercises {props.ex1 + props.ex2 + props.ex3}
     </p>
@@ -34,9 +44,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}></Header>
-      <Content part={part1} exercise={exercises1}></Content>
-      <Content part={part2} exercise={exercises2}></Content>
-      <Content part={part3} exercise={exercises3}></Content>
+      <Content name1={part1} name2={part2} name3={part3} ex1={exercises1} ex2={exercises2} ex3={exercises3}></Content>
       <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}></Total>
     </div>
   )
