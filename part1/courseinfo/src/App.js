64c0632fcc7907b037a447b0d4fae10112aa205d
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-04 13:20:33 +0300

    Completed exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,7 +4,7 @@ const Header = (props) => {
   console.log(props)
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -39,27 +39,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content array={parts}></Content>
-      <Total array={parts}></Total>
+      <Content array={course.parts}></Content>
+      <Total array={course.parts}></Total>
     </div>
   )
 }
