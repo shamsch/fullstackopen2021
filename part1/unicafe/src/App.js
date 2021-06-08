975c2a6acd58601ab0cfea49bb78e22a6c1bf293
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-08 17:41:44 +0300

    Completed exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Heading = (props) =>{
+const Heading = (props) => {
   return (
     <div>
       <h2>{props.title}</h2>
@@ -8,43 +8,43 @@ const Heading = (props) =>{
   )
 }
 
-const Buttons = (props) =>{
+const Buttons = (props) => {
   return (
-    <div style={{display: "inline"}}>
+    <div style={{ display: "inline" }}>
       <button onClick={props.clickFunction}>{props.buttonName} </button>
     </div>
   )
 }
-const Statistics = (props) =>{
+const Statistics = (props) => {
   return (
-    <div>
-      {props.name} {props.score}
-    </div>
+    <tbody>
+      <td>{props.name} </td>
+      <td>{props.score}</td>
+    </tbody>
   )
 }
 
 const DisplayStat = (props) => {
-  
-  if (props.data.all==0){
-    return(
+
+  if (props.data.all == 0) {
+    return (
       <div>
-      No feedback given
+        No feedback given
       </div>
     )
-    
+
   }
 
-  return(
-    <div>
-      <Statistics name={"good"} score={props.data.good}></Statistics>
-      <Statistics name={"bad"} score={props.data.bad}></Statistics>
-      <Statistics name={"neutral"} score={props.data.neutral}></Statistics>
-      <Statistics name={"all"} score={props.data.all}></Statistics>
-      <Statistics name={"average"} score={props.data.avg}></Statistics>
-      <Statistics name={"positive"} score={props.data.percentage+" %"}></Statistics>
-    </div>
-    
-)
+  return (
+      <table>
+        <Statistics name={"good"} score={props.data.good}></Statistics>
+        <Statistics name={"bad"} score={props.data.bad}></Statistics>
+        <Statistics name={"neutral"} score={props.data.neutral}></Statistics>
+        <Statistics name={"all"} score={props.data.all}></Statistics>
+        <Statistics name={"average"} score={props.data.avg}></Statistics>
+        <Statistics name={"positive"} score={props.data.percentage + " %"}></Statistics>
+      </table>
+  )
 }
 
 const App = () => {
@@ -52,9 +52,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  let all= good+bad+neutral
-  let avg= (good-bad)/(all-neutral)
-  let percentage= (good/all)*100
+  let all = good + bad + neutral
+  let avg = (good - bad) / (all - neutral)
+  let percentage = (good / all) * 100
   let data = {
     good: good,
     neutral: neutral,
@@ -67,9 +67,9 @@ const App = () => {
   return (
     <div>
       <Heading title={"give feedback"}></Heading>
-      <Buttons clickFunction={()=> setGood(good+1)} buttonName={"good"}></Buttons>
-      <Buttons clickFunction={()=> setBad(bad+1)} buttonName={"bad"}></Buttons>
-      <Buttons clickFunction={()=> setNeutral(neutral+1)} buttonName={"neutral"}></Buttons>
+      <Buttons clickFunction={() => setGood(good + 1)} buttonName={"good"}></Buttons>
+      <Buttons clickFunction={() => setBad(bad + 1)} buttonName={"bad"}></Buttons>
+      <Buttons clickFunction={() => setNeutral(neutral + 1)} buttonName={"neutral"}></Buttons>
       <Heading title={"statistics"}></Heading>
       <DisplayStat data={data}></DisplayStat>
     </div>
