3482954f4194ffa4d23da4c53a5fc2dcc22d875b
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-11 13:54:46 +0300

    Completed exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addNewPerson = (e) => {
+    e.preventDefault()
+    let newPersonObject= {name: newName}
+    setPersons(persons.concat(newPersonObject))
+    setNewName('')
+  }
+
+  const updateNameField = (e) =>{
+    setNewName(e.target.value)
+  }
+
+  let allName = persons.map((person, index)=> <p key={index}>{person.name}</p>)
+  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={updateNameField}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addNewPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {allName}
+      </div>
+    </div>
+  )
+}
+
+export default App
