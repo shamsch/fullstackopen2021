22c8847358c14a45360bcf7934e01233a2cc4400
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-30 01:54:28 +0300

    Completed exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,23 @@
 const dummy = (blogs) => {
   return 1; 
 };
+const totalLikes = (blogs) => {
+  const calculateSum = (sum, value) => sum+ value.likes; 
+  
+  return blogs.length? blogs.reduce(calculateSum, 0): 0;
+
+}
+
+const favoriteBlog = (blogs) => {
+  let currentFavPost=blogs[0];
+  for (let element of blogs){
+    if(element.likes>currentFavPost.likes){
+      currentFavPost= element;
+    }
+  }
+  return blogs.length? currentFavPost : null;
+}
 
 module.exports = {
-  dummy,
+  dummy, totalLikes,favoriteBlog
 };
