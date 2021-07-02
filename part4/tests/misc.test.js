6b66f69c8adfdfb8502ed1aaa2bd9b68365003cf
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-07-03 01:45:46 +0300

    Completed exercise 4.6

--- part4/tests/misc.test.js
+++ part4/tests/misc.test.js
@@ -94,3 +94,20 @@ describe("Favorite blog", () => {
   test("of a bigger list is calculated right.", () =>
     expect(listHelper.favoriteBlog(listWithManyBlogs)).toEqual(favBlog));
 });
+
+describe("Most blogs of an author", () => {
+  test("of empty list is null.", () =>
+    expect(listHelper.mostBlogs([])).toEqual(null));
+
+  test("when list has only one blog, equals the author of that.", () =>
+    expect(listHelper.mostBlogs(listWithOneBlog)).toEqual({
+      author: listWithOneBlog[0].author,
+      blogs: 1,
+    }));
+
+  test("of a bigger list is calculated right.", () =>
+    expect(listHelper.mostBlogs(listWithManyBlogs)).toEqual({
+      author: "Robert C. Martin",
+      blogs: 3,
+    }));
+});
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,23 +1,50 @@
 const dummy = (blogs) => {
-  return 1; 
+  return 1;
 };
 const totalLikes = (blogs) => {
-  const calculateSum = (sum, value) => sum+ value.likes; 
-  
-  return blogs.length? blogs.reduce(calculateSum, 0): 0;
+  const calculateSum = (sum, value) => sum + value.likes;
 
-}
+  return blogs.length ? blogs.reduce(calculateSum, 0) : 0;
+};
 
 const favoriteBlog = (blogs) => {
-  let currentFavPost=blogs[0];
-  for (let element of blogs){
-    if(element.likes>currentFavPost.likes){
-      currentFavPost= element;
+  let currentFavPost = blogs[0];
+  for (let element of blogs) {
+    if (element.likes > currentFavPost.likes) {
+      currentFavPost = element;
     }
   }
-  return blogs.length? currentFavPost : null;
-}
+  return blogs.length ? currentFavPost : null;
+};
+
+const mostBlogs = (blogs) => {
+  if(!blogs.length){
+    return null;
+  }
+  let listOfAuthors = {};
+  for (let blog of blogs) {
+    if (listOfAuthors[blog.author] !== undefined) {
+      listOfAuthors[blog.author] += 1;
+    } else {
+      listOfAuthors[blog.author] = 1;
+    }
+  }
+
+  const authors = Object.keys(listOfAuthors);
+  let authorWithMaxBlogs = authors[0];
+  let maxBlog = listOfAuthors[authorWithMaxBlogs];
+  for (let element in listOfAuthors) {
+    if (listOfAuthors[element] > maxBlog) {
+      authorWithMaxBlogs = element;
+      maxBlog = listOfAuthors[element];
+    }
+  }
+  return { author: authorWithMaxBlogs, blogs: maxBlog };
+};
 
 module.exports = {
-  dummy, totalLikes,favoriteBlog
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
 };
