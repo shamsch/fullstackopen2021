54caf2dbc6f530395ae620ab5a0628dba4eb46a3
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-02-18 01:25:00 +0200

    Exercise 4.8-4.12 done

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,23 +1,16 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (request, response) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => next(error));
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs);
 });
 
-blogRouter.post("/", (request, response) => {
+blogRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => next(error));
+  const res= await blog.save()
+  response.status(200).json(res)
 });
 
 module.exports = blogRouter;
---
+++ part4/tests/test_helper.js
@@ -0,0 +1,26 @@
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+    {
+        title: "abc",
+        author: "kkk",
+        url: "https://google.com/",
+        likes: 7,
+    },
+    {
+        title: "xyz",
+        author: "qqq",
+        url: "http://yahoo.com",
+        likes: 5,
+    },
+];
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({});
+    return blogs.map((blog) => blog.toJSON());
+};
+
+module.exports = {
+    initialBlogs,
+    blogsInDb,
+};
