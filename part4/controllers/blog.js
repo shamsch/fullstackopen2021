f09c9bb9369e1af05145337927bcf31571146628
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-02-20 02:47:06 +0200

    exercise 4.21 doen

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -49,8 +49,22 @@ blogRouter.put("/:id", async (request, response) => {
 });
 
 blogRouter.delete("/:id", async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id);
-  response.status(202).end();
+  const blog = await Blog.findById(request.params.id);
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  if (blog.user.toString() === user.id.toString()) {
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  }
+  
+  response.status(400).send("user do not have right to delete this blog").end();
 });
 
 module.exports = blogRouter;
