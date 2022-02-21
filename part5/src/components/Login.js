2f5f8840ba6dfaaed27cd5bb1c2bcb597d98a9e5
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-02-21 22:05:48 +0200

    added frontend

---
+++ part5/src/components/Login.js
@@ -0,0 +1,21 @@
+
+const Login = () => {
+  return (
+    <>
+    <h1>login to the application</h1>
+    <form>
+        <label> 
+            username
+        </label>
+        <input/>
+        <br/>
+        <label> 
+            password
+        </label>
+        <input/>
+    </form>
+    </>
+  )
+}
+
+export default Login; 
