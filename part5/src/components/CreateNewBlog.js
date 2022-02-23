535f4e46f3a308639451d52350db07b833eecfbb
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-02-23 20:10:03 +0200

    exercise 5.5 and 5.6 done

--- part5/src/components/CreateNewBlog.js
+++ part5/src/components/CreateNewBlog.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Notification } from "./Notification";
 
 const DEFAULT_BLOG = {
@@ -10,6 +10,10 @@ const DEFAULT_BLOG = {
 export const CreateNewBlog = ({ handleBlogCreate }) => {
   const [newBlog, setNewblog] = useState(DEFAULT_BLOG);
   const [showNotif, setShowNotif] = useState(false);
+
+  const [show, setShow] = useState(false);
+
+  const showStyles = { display: show ? "" : "none" };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,35 +30,47 @@ export const CreateNewBlog = ({ handleBlogCreate }) => {
 
   return (
     <div>
-      <h1>Create new</h1>
-      {showNotif ? (
-        <Notification color={"green"} text={"Blog added sucessfully"} />
-      ) : null}
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label>title</label>
-        <input
-          type="text"
-          onChange={(e) =>
-            setNewblog({ ...newBlog, title: `${e.target.value}` })
-          }
-        />
-        <br />
-        <label>author</label>
-        <input
-          type="text"
-          onChange={(e) =>
-            setNewblog({ ...newBlog, author: `${e.target.value}` })
-          }
-        />
-        <br />
-        <label>url</label>
-        <input
-          type="url"
-          onChange={(e) => setNewblog({ ...newBlog, url: `${e.target.value}` })}
-        />
-        <br />
-        <button>create</button>
-      </form>
+      {!show && <button onClick={() => setShow(true)}>new blog</button>}
+      <div style={showStyles}>
+        <h1>Create new</h1>
+        {showNotif ? (
+          <Notification color={"green"} text={"Blog added sucessfully"} />
+        ) : null}
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label>title</label>
+          <input
+            type="text"
+            onChange={(e) =>
+              setNewblog({ ...newBlog, title: `${e.target.value}` })
+            }
+          />
+          <br />
+          <label>author</label>
+          <input
+            type="text"
+            onChange={(e) =>
+              setNewblog({ ...newBlog, author: `${e.target.value}` })
+            }
+          />
+          <br />
+          <label>url</label>
+          <input
+            type="url"
+            onChange={(e) =>
+              setNewblog({ ...newBlog, url: `${e.target.value}` })
+            }
+          />
+          <br />
+          <button>create</button>
+        </form>
+        <button
+          onClick={() => {
+            setShow(false);
+          }}
+        >
+          cancel
+        </button>
+      </div>
     </div>
   );
 };
