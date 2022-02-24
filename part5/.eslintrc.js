ce2e37db82594aa453d14bb4d961b395b04d2386
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-02-25 01:00:08 +0200

    changed some linting

--- part5/.eslintrc.js
+++ part5/.eslintrc.js
@@ -17,11 +17,7 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
     eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
     "arrow-spacing": ["error", { before: true, after: true }],
     "no-console": 0,
     "react/prop-types": 0,
