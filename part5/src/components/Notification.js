bf3d810373ac10fc6c103f8cb5a73500d721b70f
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-02-25 00:48:46 +0200

    exercise 5.11 done

--- part5/src/components/Notification.js
+++ part5/src/components/Notification.js
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types'
 
 export const Notification = ({color, text}) => {
   return (
     <div style={{"background":`${color}`}}>{text}</div>
   )
+}
+
+Notification.propTypes ={
+  color: PropTypes.string.isRequired, 
+  text: PropTypes.string.isRequired
 }
