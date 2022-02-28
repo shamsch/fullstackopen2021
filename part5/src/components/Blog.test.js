5c2740ce2ca346e930cc0f13faf46942ea2d36a6
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-02-28 02:04:38 +0200

    removed unecessary imports

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import useEvent from '@testing-library/user-event'
 import {Blog} from "./Blog";
 
 const testBlog = {
