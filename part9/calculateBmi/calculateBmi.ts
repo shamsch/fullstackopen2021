79ee5d3ae10561a40900f9ae89dcb5ecec4455fb
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-04-23 17:36:57 +0300

    9.1

---
+++ part9/calculateBmi/calculateBmi.ts
@@ -0,0 +1,17 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const heightInMeter = height / 100;
+  const bmi = weight / (heightInMeter * heightInMeter);
+  if (bmi < 18.5) {
+    return "underweight (unhealthy)";
+  } else if (bmi >= 18.5 && bmi <= 22.9) {
+    return "normal range (healthy)";
+  } else if (bmi >= 23.0 && bmi <= 24.9) {
+    return "overweight (at risk)";
+  } else if (bmi >= 25.0 && bmi <= 29.9) {
+    return "overweight II (moderately obese)";
+  } else if (bmi >= 30) {
+    return "overweight III (severely obese)";
+  }
+};
+
+console.log(calculateBmi(180, 74));
