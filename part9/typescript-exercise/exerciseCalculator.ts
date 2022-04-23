c8af4884384617bad35fceb16ad9d1fd9f217226
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-04-23 20:05:27 +0300

    added interface

--- part9/typescript-exercise/exerciseCalculator.ts
+++ part9/typescript-exercise/exerciseCalculator.ts
@@ -15,6 +15,16 @@ const determineRating = (target: number, average: number) => {
 
 const ratingText = ["not so good", "kinda good", "pretty good"];
 
+interface resultObject {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
 const calculateExercise = (exerciseData: Array<number>, target: number) => {
   const days = exerciseData.length;
   const trainingDays = arrayWithoutZero(exerciseData).length;
@@ -22,7 +32,7 @@ const calculateExercise = (exerciseData: Array<number>, target: number) => {
     exerciseData.reduce((prev, current) => prev + current, 0) / days;
   const success: boolean = average >= target;
   const rating: number = determineRating(target, average);
-  return {
+  const result:resultObject = {
     periodLength: days,
     trainingDays,
     success,
@@ -31,6 +41,7 @@ const calculateExercise = (exerciseData: Array<number>, target: number) => {
     target,
     average,
   };
+  return result;
 };
 
 console.log(calculateExercise([0, 0, 0, 0, 0, 2, 1], 2));
