cdd8f8d574dd8c35e25ddfd622f1e179e18d3534
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-04-28 18:47:55 +0300

    9.12

--- part9/express-typescript/routers/patientRouter.ts
+++ part9/express-typescript/routers/patientRouter.ts
@@ -1,10 +1,25 @@
 import express from "express";
-import { getPatientDataWithoutSsn } from "../service/getData";
+import { addPatient, getPatientDataWithoutSsn } from "../service/getData";
+import { patientData } from "../types/types";
+import { validateData } from "../service/getData";
 
 const patientRouter = express.Router();
 
 patientRouter.get("/", (_req, _res) => {
     _res.send(getPatientDataWithoutSsn());
+});
+
+patientRouter.post("/", (_req, _res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const body = _req.body;
+    try {
+        const validEntry: patientData = validateData(body);
+        addPatient(validEntry);
+        _res.send(validEntry);
+    } catch (error: unknown) {
+        console.log(error);
+        _res.send(error);
+    }
 });
 
 export default patientRouter;
--- part9/express-typescript/service/getData.ts
+++ part9/express-typescript/service/getData.ts
@@ -2,9 +2,11 @@ import diagnose from "../data/diagnoses.json";
 import patient from "../data/patients.json";
 import {
     diagnoseData,
+    genderEnum,
     patientData,
     patientDataWithoutSsn,
 } from "../types/types";
+import {v1 as uuid} from "uuid";
 
 const diagnoseDataArray: diagnoseData[] = diagnose as diagnoseData[];
 const patientDataArray: patientData[] = patient as patientData[];
@@ -23,4 +25,49 @@ export const getPatientDataWithoutSsn = (): patientDataWithoutSsn[] => {
             gender,
         })
     );
+};
+
+// adding
+const validString = (str: unknown): string => {
+    if (typeof str === "string") {
+        return str;
+    } else {
+        return "";
+    }
+};
+
+const isGender = (text: any): text is genderEnum => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return Object.values(genderEnum).includes(text);
+};
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const validGender = (gender: any): genderEnum => {
+    if (isGender(gender)) {
+        return gender;
+    }
+    return genderEnum["O"];
+};
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const validateData = (data: any) => {
+    const name = validString(data.name);
+    const id = uuid();
+    const dateOfBirth = validString(data.dateOfBirth);
+    const occupation = validString(data.occupation);
+    const ssn = validString(data.ssn);
+    const gender = validGender(data.gender);
+
+    const validData: patientData = {
+        id,
+        name,
+        dateOfBirth,
+        ssn,
+        gender,
+        occupation,
+    };
+
+    return validData;
+};
+
+export const addPatient = (patient: patientData) => {
+    patientDataArray.push(patient);
 };
