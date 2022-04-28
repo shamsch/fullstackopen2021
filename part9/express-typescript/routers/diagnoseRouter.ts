1cf598934ac8e4e9794a0aae9206d33651852e74
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-04-28 14:49:19 +0300

    rename service function

--- part9/express-typescript/routers/diagnoseRouter.ts
+++ part9/express-typescript/routers/diagnoseRouter.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { getAll } from "../service/getData";
+import { getDiagnoseData } from "../service/getData";
 
 const diagnoseRouter = express.Router(); 
 
 diagnoseRouter.get('/', (_req, _res) =>{
-    _res.send(getAll());
+    _res.send(getDiagnoseData());
 });
 
 export default diagnoseRouter; 
