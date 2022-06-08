eb1bcbfb5cddf08e825756336ee5f7cd16239bb6
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-06-08 18:16:43 +0300

    8.14

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -13,89 +13,6 @@ const password= process.env.PASSWORD
 
 connection(username, password)
 
-let authors = [
-    {
-        name: "Robert Martin",
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-        born: 1952,
-    },
-    {
-        name: "Martin Fowler",
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-        born: 1963,
-    },
-    {
-        name: "Fyodor Dostoevsky",
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-        born: 1821,
-    },
-    {
-        name: "Joshua Kerievsky", // birthyear not known
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    {
-        name: "Sandi Metz", // birthyear not known
-        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-];
-
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
-
-let books = [
-    {
-        title: "Clean Code",
-        published: 2008,
-        author: "Robert Martin",
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring"],
-    },
-    {
-        title: "Agile software development",
-        published: 2002,
-        author: "Robert Martin",
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ["agile", "patterns", "design"],
-    },
-    {
-        title: "Refactoring, edition 2",
-        published: 2018,
-        author: "Martin Fowler",
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring"],
-    },
-    {
-        title: "Refactoring to patterns",
-        published: 2008,
-        author: "Joshua Kerievsky",
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring", "patterns"],
-    },
-    {
-        title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-        published: 2012,
-        author: "Sandi Metz",
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ["refactoring", "design"],
-    },
-    {
-        title: "Crime and punishment",
-        published: 1866,
-        author: "Fyodor Dostoevsky",
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ["classic", "crime"],
-    },
-    {
-        title: "The Demon ",
-        published: 1872,
-        author: "Fyodor Dostoevsky",
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ["classic", "revolution"],
-    },
-];
 
 const typeDefs = gql`
     type Book {
@@ -117,7 +34,7 @@ const typeDefs = gql`
         bookCount: Int!
         authorCount: Int!
         allBooks(author: String, genre: String): [Book!]!
-        allAuthours: [Author!]!
+        allAuthors: [Author!]!
     }
 
     type Mutation {
@@ -133,44 +50,41 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        bookCount: () => books.length,
-        authorCount: () => authors.length,
-        allBooks: (root, args) => {
+        bookCount: async () => await Book.collection.countDocuments(),
+        authorCount: async () => await Author.collection.countDocuments(),
+        allBooks: async (root, args) => {
+            const allBooks = await Book.find({}).populate("author", "name -_id");
+
             if (args.author == undefined && args.genre == undefined) {
-                return books;
+                return allBooks;
             }
             //when only author is defined
             if (!args.genre) {
-                return books.filter((book) => book.author == args.author);
+                return allBooks.filter((book) => book.author.name == args.author);
             }
             //when only genre is defined
             if (!args.author) {
-                return books.filter((book) => book.genres.includes(args.genre));
+                return allBooks.filter((book) => book.genres.includes(args.genre));
             }
 
-            return books.filter(
+            return allBooks.filter(
                 (book) =>
-                    book.author == args.author &&
+                    book.author.name == args.author &&
                     book.genres.includes(args.genre)
             );
         },
-        allAuthours: () => authors,
+        allAuthors: async () => await Author.find({}),
     },
     Author: {
-        bookCount: (root) =>
-            books.filter((book) => book.author === root.name).length,
+
+        bookCount: async (root) => {
+            const allBooks = await Book.find({}).populate("author", "name -_id"); 
+            return allBooks.filter((book) => book.author.name === root.name).length
+        }
+            
     },
     Mutation: {
         addBook: async (root, args) => {
-            // const newBook = { ...args, id: uuid() };
-            // books = [...books, newBook];
-            // if (!authors.includes(newBook.author)) {
-            //     const newAuthor = {
-            //         name: newBook.author,
-            //         id: uuid(),
-            //     };
-            //     authors = [...authors, newAuthor];
-            // }
             try {
                 let authorDb = await Author.findOne({name: args.author})
                 if(!authorDb){
@@ -185,15 +99,13 @@ const resolvers = {
                 console.log(e)
             }
         },
-        editAuthor: (root, args) => {
-            const toEdit = authors.find((x) => x.name == args.name);
+        editAuthor: async (root, args) => {
+            const toEdit = await Author.findOne({name: args.name})
             if (!toEdit) {
                 return null;
             }
-            const updatedAuthor = { ...toEdit, born: args.setBornTo };
-            authors = authors.map((author) =>
-                author.name === args.name ? updatedAuthor : author
-            );
+            const updatedAuthor = {name: toEdit.name, born: args.setBornTo };
+            const updatedAuthorDb = await Author.findByIdAndUpdate(toEdit._id, updatedAuthor);
             return updatedAuthor;
         },
     },
